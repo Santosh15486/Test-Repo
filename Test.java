27e758daae79b1b61a8fa8ea9257ef49eb1e5207
Santosh Phatangare <santoshphatangare@gmail.com>
2026-02-26 08:18:30 +0000

    test class

---
+++ Test.java
@@ -0,0 +1,44 @@
+public class Test {
+    // Sensitive information hardcoded - should be detected by security scanning
+    private static final String API_KEY = "AKIAIOSFODNN7EXAMPLE"; // secret
+    private static final String PASSWORD = "P@ssw0rd123"; // hardcoded password
+
+    public static void main(String[] args) {
+        // Unused variable, sonar issue S1068
+        int unused = 42;
+
+        // Poor exception handling, sonar issue S112
+        try {
+            riskyOperation();
+        } catch (Exception e) {
+            // swallow exception
+        }
+
+        // Weak cryptography usage SCS001
+        String encrypted = weakEncrypt("hello");
+        System.out.println(encrypted);
+
+        // Logging of sensitive data S5131
+        System.out.println("Password used: " + PASSWORD);
+
+        // Generic catch block S2486
+        try {
+            anotherRisk();
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
+    }
+
+    private static void riskyOperation() throws Exception {
+        throw new Exception("Something went wrong");
+    }
+
+    private static String weakEncrypt(String data) {
+        // dummy weak encryption
+        return "Encrypted:" + data;
+    }
+
+    private static void anotherRisk() {
+        // TODO: implement
+    }
+}
